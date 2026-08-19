a4143c9ea8fdebdfb4499cd04f5a833944d749f6
Duzc01 <552150662@qq.com>
2026-08-19 14:13:33 +0800

    release: bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.bugenzhao.mnga"
         minSdk = 26
         targetSdk = 36
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 111
+        versionName = "1.1.1"
     }
 
     signingConfigs {
